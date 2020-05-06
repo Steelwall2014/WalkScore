017d2720334de0ed77e2393baf729aa143f3ae50
Steelwall2014 <55043943+Steelwall2014@users.noreply.github.com>
2020-05-06 18:20:11 +0800

    Delete start_point.py

--- start_point.py
+++
@@ -1,9 +0,0 @@
-from osgeo import ogr
-class Startpoint:
-    def __init__(self, point_id, road_id, point_geo, walkscore=0):
-        self.point_id = point_id
-        self.road_id = road_id
-        self.point_geo = point_geo
-        self.walkscore = walkscore
-    def Update_Walkscore(self, walkscore):
-        self.walkscore = walkscore
