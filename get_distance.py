baae3ed5821183e84a53e7efb4c683014ad00c70
Steelwall2014 <55043943+Steelwall2014@users.noreply.github.com>
2020-07-10 15:12:46 +0800

    Update get_distance.py

--- get_distance.py
+++ get_distance.py
@@ -31,93 +31,6 @@ def integer(a_list):
     new_list = [int(i) for i in a_list]
     return new_list
 
-@nb.jit(nopython=True)    
-def linear_distance(point_A_coord:tuple, point_B_coord:tuple):
-    '''两点间直线距离'''
-    return math.sqrt((point_A_coord[0]-point_B_coord[0])**2 + (point_A_coord[1]-point_B_coord[1])**2)
-
-def point_to_segment_distance(point_P, point_A, point_B):
-    '''点到线段距离'''
-    point_P_x = point_P[0]
-    point_P_y = point_P[1]  
-    
-    point_A_x = point_A[0]    
-    point_A_y = point_A[1]        
-    
-    point_B_x = point_B[0]    
-    point_B_y = point_B[1]
-    
-    vector_AP = (point_P_x-point_A_x, point_P_y-point_A_y)
-    vector_AB = (point_B_x-point_A_x, point_B_y-point_A_y)
-    AB = math.sqrt((vector_AB[0]**2+vector_AB[1]**2))
-    if AB == 0:
-        return -1
-    r = (vector_AP[0]*vector_AB[0] + vector_AP[1]*vector_AB[1]) / (AB**2)
-    if r >= 1:
-        dis = linear_distance(point_B, point_P)
-    elif r <= 0:
-        dis = linear_distance(point_A, point_P)
-    else:
-        AP = math.sqrt((vector_AP[0]**2+vector_AP[1]**2))
-        cos = (vector_AP[0]*vector_AB[0] + vector_AP[1]*vector_AB[1]) / (AP*AB)
-        try:
-            sin = math.sqrt(1 - cos**2)
-        except:
-            sin = 0
-        dis = AP * sin
-    return dis
-
-def tie_point_on_road_start(point_coord, road_geo):
-    '''将出发点绑定到道路上的一个点，由于出发点本来就在道路上，所以找到出发点前后在道路线要素上的点就行'''
-    count = road_geo.GetPointCount()
-    for i in range(count-1):
-        point_A = road_geo.GetPoint_2D(i)
-        point_B = road_geo.GetPoint_2D(i+1)
-        if (point_A[0] <= point_coord[0] <= point_B[0] or point_B[0] <= point_coord[0] <= point_A[0]) and \
-           (point_A[1] <= point_coord[1] <= point_B[1] or point_B[1] <= point_coord[1] <= point_A[1]):
-            return [list(point_coord), list(point_A), list(point_B)] 
-'''
-def tie_point_on_road_stop(point_coord, MultiRoads_geos): 
-    #将停止点绑定到道路上的一个点
-    interroads = MultiRoads_geos
-    count = interroads.GetGeometryCount()
-    point_dis = {}
-    
-    for i in range(count):  #这个循环太慢了，准备改进算法或者用奇技淫巧
-        temp_road = interroads.GetGeometryRef(i)
-        point_count = temp_road.GetPointCount()
-        
-        for j in range(point_count-1):
-
-            point_A = temp_road.GetPoint_2D(j)
-            point_B = temp_road.GetPoint_2D(j+1)
-            point_P = point_coord
-
-            dis = point_to_segment_distance(point_P, point_A, point_B)
-            if dis == -1:
-                continue
-            point_dis[(point_A, point_B)] = dis
-    point_dis_order=sorted(point_dis.items(),key=lambda x:x[1],reverse=False)   
-    point_on_road1_coord = point_dis_order[0][0][0]
-    point_on_road2_coord = point_dis_order[0][0][1]
-    shortest_dis = point_dis_order[0][1]
-    
-    dis_1 = linear_distance(point_coord, point_on_road1_coord)
-    dis_2 = linear_distance(point_coord, point_on_road2_coord)
-    if abs(shortest_dis-dis_1) <= 0.01:
-        tied_point_coord = point_on_road1_coord
-    elif abs(shortest_dis-dis_2) <= 0.01:
-        tied_point_coord = point_on_road2_coord
-    else:
-        k = (point_on_road2_coord[1]-point_on_road1_coord[1])/ \
-            (point_on_road2_coord[0]-point_on_road1_coord[0])
-        b = point_on_road1_coord[1] - k*point_on_road1_coord[0]
-        tied_point_x = (k*(point_coord[1]-b) + point_coord[0])/ \
-                       (k**2 + 1)
-        tied_point_y = k*tied_point_x + b
-        tied_point_coord = (tied_point_x, tied_point_y)
-    return [list(tied_point_coord), list(point_on_road1_coord), list(point_on_road2_coord), shortest_dis]
-'''
 def tie_point_on_road_stop_discrete(point_coord:tuple, road_points:dict):  
     '''
     road_points[point_coord] = (绑定点:tuple, 前点:tuple, 后点:tuple, 距离)
@@ -130,9 +43,6 @@ def tie_point_on_road_stop_discrete(point_coord:tuple, road_points:dict):
         else:
             points_stop.append(value)
     return points_stop
-   
-def GenerateRoadPoints(MultiType_poi_points:dict, layer, seg_length:float):
-    pass
 
 def _weight_function(G, weight):
     if callable(weight):
@@ -200,19 +110,19 @@ def _dijkstra_multisource(G, sources, weight, pred=None, paths=None,
 
 
 def my_dijkstra_path_length(G, source, target, cutoff=None, weight='weight'):
+    '''在networkx的迪杰斯特拉算法的基础上小改了一下，原来到达cutoff后会报错，我改成了返回一个大值(1000000000)'''
     if source == target:
         return 0
     weight = _weight_function(G, weight)
     length = _dijkstra(G, source, weight, cutoff=cutoff, target=target)
     return length
         
-
   
 def GetNetDistances(start_point:tuple, stop_points, pre_nex_points, road_points, G):
     '''计算路网距离'''
     a = time.time()
     net_distances = []
-    #points_start = tie_point_on_road_start(start_point, road_geo)
+
     tied_start_point_coord = tuple(integer(start_point))
     start_between_A = tuple(integer(pre_nex_points[0]))
     start_between_B = tuple(integer(pre_nex_points[1]))
@@ -220,7 +130,6 @@ def GetNetDistances(start_point:tuple, stop_points, pre_nex_points, road_points,
     poi_and_tied_stop_point = {}
     for stop_point in stop_points:
         aaa = time.time()
-        #points_stop = tie_point_on_road_stop(stop_point, MultiRoads_geos)    
         points_stop = tie_point_on_road_stop_discrete(stop_point, road_points)    
         
         tied_stop_point_coord = tuple(integer(points_stop[0]))
@@ -247,30 +156,7 @@ def GetNetDistances(start_point:tuple, stop_points, pre_nex_points, road_points,
         temp_dist = temp_dist + poi_to_road_dists[stop_point]
         if temp_dist < 2400:
             net_distances.append(temp_dist)
-    '''
-    try:
-    #if nx.has_path(G, tied_start_point_coord, tied_stop_point_coord):
-        aa = time.time()
-        #注意：可能要写一个if来判断2400-poi_to_road_dis是正数还是负数
-        dist = my_dijkstra_path_length(G, tied_start_point_coord, tied_stop_point_coord, cutoff=2400-poi_to_road_dis)+poi_to_road_dis
-        #print(dist)
-        if dist < 2400:
-            net_distances.append(dist)
-        bb = time.time()
-        #print('net dist: %.6f' % (bb-aa))
-        #print('路网距离计算成功')
-    except:
-    #else:
-        aaaa = time.time()
-        net_distances.append(linear_distance(start_point, stop_point))
-        bbbb = time.time()
-        #print('linear dist: %.6f' % (bbbb-aaaa))
-        #print('路网距离计算失败，使用直线距离')
-    '''
     bbb = time.time()
-    #print('time: %.6f' % (bbb-aaa))
-    #当路网距离无法进行计算时会消耗大量时间
-    #可能需要在最后的时候把新添加的边删掉
     b = time.time()
     #print('all time: %.6f' % (b-a))
     return net_distances
