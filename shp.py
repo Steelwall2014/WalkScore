9d09d0f80dd2cc415d24ea5ae96aec35e257ca7c
Steelwall2014 <55043943+Steelwall2014@users.noreply.github.com>
2020-07-10 15:03:41 +0800

    Update shp.py

--- shp.py
+++ shp.py
@@ -3,7 +3,7 @@
 Created on Tue May 12 10:52:34 2020
 
 @author: 张径舟
-路径：D:\Python\Lib\shp.py
+shp文件的读写比较麻烦，所以自己写了一个读写shp文件的模块，模仿csv模块
 """
 import ogr
 import gdal
@@ -100,32 +100,6 @@ class shp_writer():
             feature.SetGeometry(row[-1])
             self.layer.CreateFeature(feature) 
             self.ds.SyncToDisk()
-            
-
-        
-'''
-def __transform(self, geom, transform):
-    target_srs = None
-    src_srs = _temp_row[geom_index].GetSpatialReference()
-    if type(target_spatial_reference) is int:
-        epsg = target_spatial_reference
-        target_srs = osr.SpatialReference()
-        target_srs.ImportFromEPSG(epsg)
-    elif type(target_spatial_reference) is str:
-        epsg = None
-        if target_spatial_reference.isdigit():
-            epsg = int(target_spatial_reference)
-        target_srs = osr.SpatialReference()
-        if epsg:
-            target_srs.ImportFromEPSG(epsg)
-    elif type(target_spatial_reference) == type(osr.SpatialReference()):
-        target_srs = target_spatial_reference
-    
-    if target_srs != src_srs:
-        ct = osr.CoordinateTransformation(src_srs, target_srs)
-    else:
-        ct = None
-'''   
         
 class shp_reader():
     
@@ -213,146 +187,6 @@ class shp_reader():
             else:
                 fields.append('')
         return fields
-
-#gdal.SetConfigOption('SHAPE_ENCODING', 'GBK')
-#'C:\Users\DELL\Desktop\银川.shp'
-#'D:\all_cities\OSM路网_投影\南京.shp'
-if __name__ == '__main__':
-    '''
-    reader1 = shp_reader('C:\\Users\\DELL\\Desktop\\test\\000.shp')
-    rows1 = list(reader1.readrows())
-    '''
-    '''
-    reader = shp_reader('D:\\黄土\\result1_Update.shp')
-    head = reader.readhead()
-    srs = reader.readSRS()
-    rows = reader.readrows()
-    new_rows = []
-    yuan = ogr.Geometry(ogr.wkbMultiPolygon)
-    yuan.AssignSpatialReference(srs)
-    for row in rows:
-        if row[27] == 0:
-            geom = row[-1]
-            yuan.AddGeometry(geom)
-    new_rows.append([yuan])
-    print(yuan.Area())
-    #writer = shp_writer('C:\\Users\\DELL\\Desktop\\test\\only_yuan_multipoly.shp')
-    #writer.writerows(head, new_rows)
-    '''
-    '''
-    reader = shp_reader('D:\\黄土\\result1_Update.shp')
-    head = reader.readhead()
-    srs = reader.readSRS()
-    rows = reader.readrows()
-    new_rows = []
-    for row in rows:
-        if row[27] == 0:
-            new_rows.append(row)  
-    writer = shp_writer('C:\\Users\\DELL\\Desktop\\test\\only_yuan.shp')
-    writer.writerows(head, new_rows)
-    '''
-    
-    '''
-    reader = shp_reader('C:\\Users\\DELL\\Desktop\\test\\001.shp')
-    rows = reader.readrows()
-    a = 0
-    for row in rows:
-        geom = row[-1]
-        a += geom.Area()
-        area = row[-2]
-        print(area, geom.Area())
-    print(a)
-    '''
-    '''
-    r = shp_reader('C:\\Users\\DELL\\Desktop\\test\\yuan.shp')
-    w = shp_writer('C:\\Users\\DELL\\Desktop\\test\\yuan1.shp')
-    h = r.readhead()
-    nr = []
-    h['area_ogr'] = ogr.OFTReal
-    for row in r.readrows():
-        geom = row[-1]
-        area = geom.Area()
-        new_row = row
-        new_row.insert(-1, area)
-        nr.append(new_row)
-    w.writerows(h, nr)    
-    '''
-    """
-    r = shp_reader('C:\\Users\\DELL\\Desktop\\test\\yuan.shp')
-    w = shp_writer('C:\\Users\\DELL\\Desktop\\test\\yuan_dis_ogr.shp')
-    srs = r.readSRS()
-    yuan = ogr.Geometry(ogr.wkbMultiPolygon)
-    yuan.AssignSpatialReference(srs)
-    for row in r.readrows():
-        geom = row[-1]
-        yuan.AddGeometryDirectly(geom)
-    h = []
-    nr = [[yuan]]
-    #w.writerows(h, nr)
-    r1 = shp_reader('C:\\Users\\DELL\\Desktop\\test\\yuan_dis_ogr.shp')    
-    for row in r1.readrows():
-        yuan1 = row[-1]
-    a = []
-    for i in range(yuan.GetGeometryCount()):
-        a.append(yuan.GetGeometryRef(i).Area())
-    a1 = []
-    for i in range(yuan1.GetGeometryCount()):
-        a1.append(yuan1.GetGeometryRef(i).Area())
-    print(len(a1))
-    """
-    '''
-    yuan_wkt = yuan.ExportToIsoWkt()
-    yuan1_wkt = yuan1.ExportToIsoWkt()
-    with open('C:\\Users\\DELL\\Desktop\\test\\111.txt', 'w') as file:
-        file.write(yuan_wkt)
-        file.write('\n')
-        file.write(yuan1_wkt)  
-    '''
-    #print(yuan.GetGeometryRef(2).Equals(yuan1.GetGeometryRef(2)))
-    #print(yuan.GetGeometryRef(200))
-    #print(yuan1.GetGeometryRef(200))
-    '''
-    r1 = shp_reader('C:\\Users\\DELL\\Desktop\\test\\only_yuan_multipoly.shp')
-    r2 = shp_reader('C:\\Users\\DELL\\Desktop\\test\\only_yuan.shp')
-    r3 = shp_reader('C:\\Users\\DELL\\Desktop\\test\\only_yuan_dissolve.shp')
-    w1 = shp_writer('C:\\Users\\DELL\\Desktop\\test\\only_yuan_multipoly1.shp')
-    w2 = shp_writer('C:\\Users\\DELL\\Desktop\\test\\only_yuan1.shp')
-    w3 = shp_writer('C:\\Users\\DELL\\Desktop\\test\\only_yuan_dissolve1.shp')
-    h1 = r1.readhead()
-    h1['area_ogr'] = ogr.OFTReal
-    h2 = r2.readhead()
-    h2['area_ogr'] = ogr.OFTReal
-    h3 = r3.readhead()
-    h3['area_ogr'] = ogr.OFTReal
-    nr1 = []
-    nr2 = []
-    nr3 = []
-    
-    for row in r1.readrows():
-        geom = row[-1]
-        area = geom.Area()
-        new_row = row
-        new_row.insert(-1, area)
-        nr1.append(new_row)
-    w1.writerows(h1, nr1)
-    
-    for row in r2.readrows():
-        geom = row[-1]
-        area = geom.Area()
-        new_row = row
-        new_row.insert(-1, area)
-        nr2.append(new_row)
-    w2.writerows(h2, nr2)
-    
-    for row in r3.readrows():
-        geom = row[-1]
-        area = geom.Area()
-        new_row = row
-        new_row.insert(-1, area)
-        nr3.append(new_row)
-    w3.writerows(h3, nr3)
-    '''
-
         
         
     
